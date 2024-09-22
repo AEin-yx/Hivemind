caf74f654bc76a6033671ea78351492ab2d7c406
AEin-yx <aeinha.sha@gmail.com>
2024-09-23 00:27:44 +0530

    feat: Implement functional user sign-up endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
-import "userRouter" from "./routers/user";
-import "workerRouter" from "./routers/worker";
-
+import userRouter from "./routers/user";
+import workerRouter from "./routers/worker";
 const app=express();
-
+require('dotenv').config();
 app.use("/v1/user",userRouter);
 app.use("/v1/worker",workerRouter);
+
+app.listen(3000,()=>{
+    console.log("DATABASE_URL:", process.env.DATABASE_URL)
+});
--- backend/src/routers/user.ts
+++ backend/src/routers/user.ts
@@ -1,8 +1,40 @@
+import { PrismaClient } from "@prisma/client";
 import {Router} from "express";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET="union12";
 const router=Router();
+const prismaClient=new PrismaClient();
 
-router.post("/signin",(req,res)=>{
+router.post("/signin", async(req,res)=>{
+    const hardcodedWalletAddress="BU3h5q4a5zUQMj75VGEKJbx3bjq8e8TZJo3orbowTutB";
+    const existingUser=await prismaClient.user.findFirst({
+        where:{
+            address:hardcodedWalletAddress
+        }
+    })
 
+    if(existingUser){
+        const token=jwt.sign({
+            userId:existingUser.id
+        },JWT_SECRET);
+
+        res.json({
+            token
+        })
+    }else{
+        const user=await prismaClient.user.create({
+            data: {
+                address:hardcodedWalletAddress
+            }
+        })
+        const token=jwt.sign({
+            userId:user.id
+        },JWT_SECRET);
+        res.json({
+            token
+        })
+    }
 });
 
 export default router;
